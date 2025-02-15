4d159fd988540fee1cc4d59934b72c8aa991bfdf
reinhardbuyabo <buyabor@gmail.com>
2025-02-15 19:58:14 +0300

    Initial Commit

---
+++ Apple PieTests/Apple_PieTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_PieTests.swift
+//  Apple PieTests
+//
+//  Created by Reinhard Buyabo on 15/02/2025.
+//
+
+import Testing
+@testable import Apple_Pie
+
+struct Apple_PieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
